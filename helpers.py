e92f2a8c75506c30b1770e5f078fe03fdeb9367e
Stephan <stephan.niebling@gmail.com>
2018-06-11 16:22:05 +0200

    Added functions to read and plot vibrational spectra based on Turbomole calculations

--- helpers.py
+++ helpers.py
@@ -11,8 +11,9 @@ sys.path.insert(0,'/home/snieblin/work/')
 import libspec
 import scipy.signal as ssi
 
-def gauss(x, *params):
+def mult_gauss(x, *params):
     '''
+    Multiple gaussian function
     Inputs x values and gaussian parameters
     Outputs y values
     '''
@@ -24,8 +25,9 @@ def gauss(x, *params):
         y = y + amp * np.exp( -((x - ctr)/wid)**2)
     return y
 
-def lorentz(x, *params):
+def mult_lorentz(x, *params):
     '''
+    Multiple lorentzian function
     Inputs x values and gaussian parameters
     Outputs y values
     '''
@@ -36,3 +38,118 @@ def lorentz(x, *params):
         wid = params[i+2]
         y = y + tau * wid / (2*np.pi) / ((x-ctr)**2+(wid/2)**2)
     return y
+
+def gauss(x,center,height,fwhm=10):
+    """ Single gaussian function                                     
+    """
+    c=fwhm/(2*np.sqrt(2*np.log(2)))
+    return height*np.exp(-np.subtract(x,center)**2/(2*(c**2))\
+)
+
+def extract_int(fn, scalf=1):
+    '''
+    Extracts intensities from Turbomole
+    intense.out file
+    fn: file name
+    scalf: frequency scaling factor
+    '''
+    # Open file
+    with open(fn) as f:
+        s = f.readlines()
+    # extract lines with frequencies and intensities
+    freq=[]
+    ir_int=[]
+    raman_parpar, raman_ortort, raman_ortunpol=[], [], []
+    # Extract data
+    for i in range(0,len(s)):
+        if '     frequency' in s[i]:
+            tmpline=s[i].split()
+            del tmpline[0]
+            freq.extend(tmpline)
+        if 'intensity (km/mol)  ' in s[i]:
+            tmpline=s[i].split()
+            del tmpline[0]
+            del tmpline[0]
+            ir_int.extend(tmpline)
+        if  '    (par,par)' in s[i]:
+            tmpline=s[i].split()
+            del tmpline[0]
+            raman_parpar.extend(tmpline)
+        if  '    (ort,ort)' in s[i]:
+            tmpline=s[i].split()
+            del tmpline[0]
+            raman_ortort.extend(tmpline)
+        if  '    (ort,unpol)' in s[i]:
+            tmpline=s[i].split()
+            del tmpline[0]
+            raman_ortunpol.extend(tmpline)
+    # Delete imaginary frequencies
+    counter=0
+    while "i" in freq[counter]:
+        print("Imaginary frequency detected! "+ freq[counter])
+        freq[counter]=freq[counter].replace('i','-')
+        counter+=1
+    # Delete rotations and translations
+    freq[0:6]=[]
+    ir_int[0:6]=[]
+    raman_parpar[0:6]=[]
+    raman_ortort[0:6]=[]
+    raman_ortunpol[0:6]=[]
+    # Create arrays
+    print("Scaling factor for last column: " + str(scalf))
+    freq = np.array(list(map(float,freq)))
+    freq_scalf = freq * scalf
+    raman_parpar = np.array(list(map(float,raman_parpar)))
+    raman_ortort = np.array(list(map(float,raman_ortort)))
+    raman_ortunpol = np.array(list(map(float,raman_ortunpol)))
+    ir_int = np.array(list(map(float,ir_int)))
+    #freq_scalf = scalf * freq
+    # Create structured array
+    out_array = np.zeros(len(freq), 
+                         dtype=[('freq', 'float32'), 
+                                ('freq_scalf', 'float32'), 
+                                ('ir_int', 'float32'), 
+                                ('raman_parpar', 'float32'),
+                                ('raman_ortort', 'float32'),
+                                ('raman_ortunpol', 'float32')])
+    out_array['freq'] = freq
+    out_array['freq_scalf'] = freq_scalf
+    out_array['ir_int'] = ir_int
+    out_array['raman_parpar'] = raman_parpar
+    out_array['raman_ortort'] = raman_ortort
+    out_array['raman_ortunpol'] = raman_ortunpol
+    return out_array
+    
+def create_spec(ints, x=np.linspace(1000,1900,1000), which_int='raman_parpar'):
+    '''
+    This inputs a list of frequencies and intensities and
+    outputs an array with the individual gaussian curves 
+    '''
+    # Initialize array
+    arr = np.zeros((len(x), len(ints)))
+    # Decide whether to take Raman or IR
+    # Plot spectra                                                
+    for i in range(0,len(ints['freq_scalf'])):
+        arr[:,i]= gauss(x,ints['freq_scalf'][i],ints[which_int][i])
+    return arr
+
+def plot_spec(ax, x, spec, which_int='raman_parpar', color='blue'):
+    '''
+    This plots a spectrum
+    with annotated frequencies
+    ax: axis handle
+    x_spec:  x-values
+    spec: spectrum (y-values)
+    which_int: 'ir_int', 'raman_ortort', 'raman_parpar' or 'raman_ortunpol'
+    '''
+    # Plot model
+    for i in range(0,spec.shape[1]):
+        ax.plot(x, spec[:,i], '--', color='grey')
+        # Determine maximum for each curve
+        if np.max(spec[:,i]) > 0:
+            y_max = np.max(spec[:,i])
+            x_max = x[np.argmax(spec[:,i])]
+            # Add labels
+            ax.annotate("{:.0f}".format(x_max),xy=(x_max,y_max),ha='center')
+    ax.plot(x, np.sum(spec, axis=1), color='blue')
+    return None
