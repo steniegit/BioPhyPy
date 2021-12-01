5bcb993c3ceb195310499f631d5e69c83cd14973
stephan <stephan.niebling@gmail.com>
2021-12-01 16:57:41 +0100

    Added contrast plot+fit

--- MassPhoto.py
+++ MassPhoto.py
@@ -40,6 +40,7 @@ class MP_data:
                           'weighted_width': 50,
                           'tol': 50,
                           'guess_pos': [66, 148, 480]}
+        self.fit_type  = 'None'
         # Mock table
         self.fit_table = pd.DataFrame()
         return None
@@ -147,6 +148,25 @@ class MP_data:
         show_label: Shows gaussian paramters for each component 
         contrast: Show contrast instead of masses in kDa
         '''
+        # Set right quantity based on choice in contrasts
+        if contrasts:
+            counts = self.hist_counts_contrasts
+            centers = self.hist_centers_contrasts
+            binwidth = self.hist_binwidth_contrasts
+            # Check if fit results are available
+            if self.fit_type == 'contrasts':
+                plot_fit = True
+            else:
+                plot_fit = False
+        else:
+            counts = self.hist_counts
+            centers = self.hist_centers
+            binwidth = self.hist_binwidth
+            # Check if fit results are available
+            if self.fit_type == 'masses':
+                plot_fit = True
+            else:
+                plot_fit = False
         # Create fig
         if ax==None:
             fig, ax = plt.subplots(1)
@@ -154,13 +174,41 @@ class MP_data:
             fig = plt.gcf()
         # Plot it
         if contrasts:
-            ax.bar(self.hist_centers_contrasts, self.hist_counts_contrasts, alpha=.5, width=self.hist_binwidth_contrasts)
+            ax.set_xlabel('Contrast')
         else:
-            ax.bar(self.hist_centers, self.hist_counts, alpha=.5, width=self.hist_binwidth)
-        ax.set_xlabel('Mass / kDa')
+            ax.set_xlabel('Mass / kDa')
         ax.set_ylabel('Counts')
-        # Plot fit if there
+        # Plot histogram
+        ax.bar(centers, counts, alpha=.5, width=binwidth)
+
+        # Check and set xlim
+        # Sanity check for xlim if contrasts are used
+        xlim = np.sort(xlim)
+        if contrasts:
+            # Set xlim after sanity check            
+            if np.max(xlim) > 1:
+                # Contrast cannot be beyond 2
+                xlim = [np.min(centers), np.max(centers)]
+            ax.set_xlim(xlim)                    
+            # Invert xaxis
+            ax.invert_xaxis()
+        else:
+            ax.set_xlim(xlim)       
+        # Set xlim
+
+        # Determine indices
+        lower_ind = np.argmin(np.abs(centers-xlim[0]))
+        upper_ind = np.argmin(np.abs(centers-xlim[1]))
+        inds = np.sort([lower_ind, upper_ind])
+        # Determine ylim
         if hasattr(self, 'fit'):
+            # Increase ylim to have space for labels
+            if show_labels:
+                ax.set_ylim([0, np.max(counts[inds[0]:inds[1]])*1.35])
+            else:
+                ax.set_ylim([0, np.max(counts[inds[0]:inds[1]])*1.1])
+        # And also fits (if available)
+        if plot_fit:
             #ax.plot(self.fit[:,0], self.fit[:,1:-1], linestyle='--', color='C1')
             ax.plot(self.fit[:,0], self.fit[:,-1], linestyle='-', color='C1', alpha=1)
             for i in range(int(len(self.popt)/3)):
@@ -175,54 +223,64 @@ class MP_data:
                 # Plot individual gaussians
                 ax.plot(self.fit[:,0], self.fit[:,i+1] , color='C1', alpha=1, linestyle='--')
                 # Determine area under curve
-                auc = np.trapz(self.fit[:,i+1], x=self.fit[:,0]) / np.diff(self.hist_centers)[0]
+                auc = np.trapz(self.fit[:,i+1], x=self.fit[:,0]) / np.diff(centers)[0]
                 # Add label
                 if show_labels:
-                    ax.text(pos, height+0.05*np.max(self.hist_counts), "%.0f kDa\n$\sigma=%.0f\,$kDa\n%.0f$\,$counts \n(%.0f%%)" % (pos, width/2/np.sqrt(2*np.log(2)), auc, auc/self.n_binding*100), ha='center', va='bottom')
+                    if contrasts:
+                        text_label = "%.1e \n$\sigma=%.1e\,$\n%.0f$\,$counts \n(%.0f%%)" % (pos, width/2/np.sqrt(2*np.log(2)), auc, auc/self.n_binding*100)
+                    else:
+                        text_label = "%.0f kDa\n$\sigma=%.0f\,$kDa\n%.0f$\,$counts \n(%.0f%%)" % (pos, width/2/np.sqrt(2*np.log(2)), auc, auc/self.n_binding*100)
+                    ax.text(pos, height+0.05*np.max(self.hist_counts), text_label , ha='center', va='bottom')
             if plot_weights:
                 ax.plot(self.hist_centers, self.weights * np.max(self.hist_counts), color='k')
-
-        # Set limits
-        if contrasts:
-            pass
-        else:
-            if len(xlim)==0:
-                ax.set_xlim([0, np.max(self.hist_centers)])
-            else:
-                ax.set_xlim(xlim)
-            if hasattr(self, 'fit'):
-                # Increase ylim to have space for labels
-                if show_labels:
-                    ax.set_ylim([0, np.max(self.hist_counts)*1.35])
-            else:
-                ax.set_ylim([0, np.max(self.hist_counts)*1.1])
         fig.tight_layout()
         # Get axis dimension
         x_border = ax.get_xlim()[1]
         y_border = ax.get_ylim()[1]
         # Print number of counts
         ax.text(x_border*.99, y_border*.99, "Total counts: %i\nBinding: %.0f%%\nUnbinding: %.0f%%" % (self.n_counts, self.n_binding/self.n_counts*100, self.n_unbinding/self.n_counts*100), va='top', ha='right')
-        return fig
+        return fig, ax
     
-    def fit_histo(self, guess_pos=[], tol=100, max_width=200, weighted=False, weighted_width=200):
+    def fit_histo(self, xlim=[], guess_pos=[], tol=100, tol_contrasts = 0.05, max_width=200, max_width_contrasts=0.005, weighted=False, weighted_width=200, weighted_width_contrasts=0.005, contrasts=False):
         '''
         Fit gaussians to histogram
+        xlim: fit range
         guess: list with guessed centers, defines the number of gaussians to be used, 
                if empty, it will use the maximum of the histogram as guess
         weighted: Use weights around start positions, this can be used if there is a broad background
         max_width: Maximum FWHM for fitted gaussians
         weighted_width: FWHM for weights
+        contrasts: fit contrasts instead of masses
         '''
+        # Depending on contrasts select the right quantity
+        if contrasts:
+            centers = self.hist_centers_contrasts
+            counts = self.hist_counts_contrasts
+            # Use tolerance for contrasts
+            tol = tol_contrasts
+            max_width = max_width_contrasts
+            weighted_width = weighted_width_contrasts
+        else:
+            centers = self.hist_centers
+            counts = self.hist_counts
+        # Determine indices for xlim
+        if len(xlim) == 0:
+            ind_range = [0, len(centers)]
+        else:
+            xlim = np.sort(xlim)
+            ind_range = []
+            ind_range.append(np.argmin(np.abs(centers - xlim[0])))
+            ind_range.append(np.argmin(np.abs(centers - xlim[1])))
+            ind_range = np.sort(ind_range)
+        # Adjust centers and contrasts
+        centers = centers[ind_range[0]:ind_range[1]]
+        counts  = counts[ind_range[0]:ind_range[1]] 
         # If no guess are taken, only fit one gaussian and use maximum in histogram as guess
         if len(guess_pos) == 0:
-            #guess_pos = self.hist_centers[np.argmax(self.hist_counts)]
-            #guess_amp = np.max(self.hist_counts)
-            #fit_guess = (guess_pos, guess_amp, 50)
-            #bounds = ((guess_pos-tol, 0, 0), (guess_pos+tol, np.max(self.hist_counts), max_width))
             print("No guess positions given (guess_pos)! Will try to find maxima.")
-            peaks, info = ssi.find_peaks(self.hist_counts, prominence=0.1*np.max(self.hist_counts))
-            guess_pos = self.hist_centers[peaks]
-            guess_amp = self.hist_counts[peaks]
+            peaks, info = ssi.find_peaks(counts, prominence=0.1*np.max(counts))
+            guess_pos = centers[peaks]
+            guess_amp = counts[peaks]
             print(guess_pos)
             print(guess_amp)
             if len(guess_pos) == 0:
@@ -232,32 +290,28 @@ class MP_data:
             # Get amplitude for each guess position
             guess_amp = []
             for pos in guess_pos:
-                ind = np.argmin(np.abs(self.hist_centers - pos))
-                guess_amp.append(self.hist_counts[ind])
+                ind = np.argmin(np.abs(centers - pos))
+                guess_amp.append(counts[ind])
         fit_guess = np.column_stack((np.array(guess_pos), np.array(guess_amp), np.array([.5*max_width]*len(guess_pos)))).flatten()
         lower_bounds = np.column_stack((np.array(guess_pos) - tol , np.array([0]*len(guess_pos)), np.array([0]*len(guess_pos)))).flatten()
-        upper_bounds = np.column_stack((np.array(guess_pos) + tol , np.array([np.max(self.hist_counts)]*len(guess_pos)), np.array([max_width]*len(guess_pos)))).flatten()
+        upper_bounds = np.column_stack((np.array(guess_pos) + tol , np.array([np.max(counts)]*len(guess_pos)), np.array([max_width]*len(guess_pos)))).flatten()
         bounds = (tuple(lower_bounds), tuple(upper_bounds))
         # Determine function
         func = multi_gauss
         # Set weights
         if weighted:
             print("Will do weighted fit")
-            #sigma_params = np.column_stack((np.array(guess_pos), np.array([1]*len(guess_pos)), np.array([weighted_width]*len(guess_pos)))).flatten()
-            #sigma = func(self.hist_centers, *sigma_params)
-            #sigma = (np.max(sigma) - sigma) + np.finfo(float).eps
-            #self.weights = sigma
-            sigma = delta(self.hist_centers, guess_pos, np.array([max_width]*len(guess_pos))) * np.max(self.hist_counts) + np.finfo(float).eps
+            sigma = delta(centers, guess_pos, np.array([max_width]*len(guess_pos))) * np.max(counts) + np.finfo(float).eps
         else:
-            sigma = np.ones((len(self.hist_centers)))*np.finfo(float).eps
+            sigma = np.ones((len(centers)))*np.finfo(float).eps
         # Write sigma to instance
         self.weights = sigma
         # Do fit
-        self.popt, self.pcov = curve_fit(func, self.hist_centers, self.hist_counts, p0=fit_guess, bounds=bounds, sigma=sigma)  #, method='dogbox', maxfev=1E5)
+        self.popt, self.pcov = curve_fit(func, centers, counts, p0=fit_guess, bounds=bounds, sigma=sigma)  #, method='dogbox', maxfev=1E5)
         print(self.popt)
         # Create fit and individual gaussians for plotting
         # Finer grid
-        x = np.linspace(np.min(self.hist_centers), np.max(self.hist_centers), 1000)
+        x = np.linspace(np.min(centers), np.max(centers), 1000)
         single_gauss = []
         for i in range(0, len(self.popt), 3):
             ctr = self.popt[i]
@@ -270,6 +324,11 @@ class MP_data:
         self.fit = np.column_stack((x, np.array(single_gauss).T, fit_sum))
         # Errors
         self.fit_error = np.sqrt(np.diag(self.pcov))
+        # Set fit type
+        if contrasts:
+            self.fit_type = 'contrasts'
+        else:
+            self.fit_type = 'masses'
         # Create fit table
         self.create_fit_table()
         return None
